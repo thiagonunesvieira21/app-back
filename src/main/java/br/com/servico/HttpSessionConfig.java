d6e2229c8591712fd6477ef9934669c06067ea3b
Thiago Nunes Vieira <thiagonunesvieira21@gmail.com>
2017-11-03 16:31:35 -0200

    Alteração do mecanismo de http session de redis para server session

--- src/main/java/br/com/servico/HttpSessionConfig.java
+++ src/main/java/br/com/servico/HttpSessionConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisServer;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.session.MapSessionRepository;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import redis.clients.jedis.Protocol;
 
@@ -13,13 +15,11 @@ import java.io.IOException;
  * Created by thiago on 21/08/17.
  */
 @Configuration
-@EnableRedisHttpSession
+@EnableSpringHttpSession
 public class HttpSessionConfig {
-    private static RedisServer redisServer;
 
     @Bean
-    public JedisConnectionFactory connectionFactory() throws IOException {
-        redisServer = new RedisServer(Protocol.DEFAULT_HOST, Protocol.DEFAULT_PORT);
-        return new JedisConnectionFactory();
+    public MapSessionRepository sessionRepository() {
+        return new MapSessionRepository();
     }
 }
